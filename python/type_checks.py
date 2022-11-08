4fb39dd896a930f2b6337ed5ebf7520f0f587d8e
Shamanth <108264129+shamanth-deepsource@users.noreply.github.com>
2022-11-08 11:45:05 +0530

    Test pull_request -> latest_analysis_run
    Signed-off-by: Shamanth <108264129+shamanth-deepsource@users.noreply.github.com>

--- python/type_checks.py
+++ python/type_checks.py
@@ -5,6 +5,5 @@ def greet_all(names: list[str]) -> None:
 if __name__ == "__main__":
     heights = [5.5, 6, 5.9]
     greet_all(heights)
-# PR to master 1
-# Commit to master 1
-# Commit to master 2
+
+# Test pull_request -> latest_analysis_run
